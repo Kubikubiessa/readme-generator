b414a174bea8f02d00f7ceb2d0a9a429cc73e107
Heidrun Kubiessa <heidrunkubiessa@Heidruns-MacBook-Air.local>
2022-11-09 11:49:37 -0700

    started CLI code in index.js

--- Develop/index.js
+++ Develop/index.js
@@ -1,7 +1,30 @@
 // TODO: Include packages needed for this application
+const fs = require('fs');
+const inquirer = require('inquirer');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        name: 'title',
+        message: 'What is the title of your project?',
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'Please describe your project',
+    },
+    {
+        type: 'input',
+        name: 'installation instructions',
+        message: 'Please provide a detailed instruction of installing this project.',
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'What is your application used for?',
+    }
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
